4400bd722024c076cc48e935c3fef68d6ecde87b
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-06-02 13:38:31 +0300

    Restoring information in textboxes on edit button click

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,5 @@
 // Add todo div elements
-var btn_add = $(".add-btn");
+var btn_add = $(".add");
 //Single todo elements
 var single_todo = $(".one-todo");
 var isDone = $(".check").is(':checked');
@@ -7,39 +7,50 @@ var desc_td = $(".desc-td");
 var date_td = $(".date-td");
 var point_td = $(".point-td");
 var delete_li = $(".delete")
-
+var data = [];
+var current_li = 0;
 var id = 0;
-cuurent_todo = 1;
-$(".add").click(function(){
-    var title = $("#title").val();
-    var desc = $("#desc").val();
-    var priority = $("#point").val();
+btn_add.click(function(){
     // Get Date
     var createdAt = new Date;
-    createdAt = String(createdAt.getDate()).padStart(2,0) + '/' + String(createdAt.getMonth()+1).padStart(2,0) + "/" +createdAt.getFullYear();
+    // createdAt = String(createdAt.getDate()).padStart(2,0) + '/' + String(createdAt.getMonth()+1).padStart(2,0) + "/" +createdAt.getFullYear();
     console.log(createdAt);
+
     // Generate random id
     id = Math.floor(Math.random()*1000);
     console.log(id);
-    // 
-    $('ul').append('<li class="one-todo" id="'+id+'">'
-    +'<span class="title-td">'+title+'</span>'
+
+    // Data of current li
+    var current_li_data = [createdAt,id,$("#title").val(),$("#desc").val(),$("#point").val(),current_li]
+    data.push(current_li_data);
+    // Append new li
+    $('ul').append('<li class="one-todo" id="'+data[current_li][1]+'">'
+    +'<span class="title-td">'+data[current_li][2]+'</span>'
     +'<br> <br> <div> <input type="radio" class="check-td">'
-    +'<span class="desc-td">'+desc+'</span>'
-    +'<span class="delete"><i class="fa-solid fa-trash-can" lid="'+id+'"></i></span>'
-    +'<span class="delete"><i class="fa-solid fa-pen"></i></span>'
-    +'<span class="date-td">'+createdAt+'</span>'
-    +'<span class="point-td">'+priority+'</span>'
+    +'<span class="desc-td">'+data[current_li][3]+'</span>'
+    +'<span class="delete"><i class="fa-solid fa-trash-can" data="'+data[current_li][1]+'"></i></span>'
+    +'<span class="delete"><i class="fa-solid fa-pen" id="'+current_li+'"></i></span>'
+    // +'<span class="date-td">'+createdAt+'</span>'
+    +'<span class="point-td">'+data[current_li][4]+'</span>'
     +'</li>');
-    $("#title").text("");
-    $("#desc").text("");
-    $("#point").text("");
+    $("#title").val("");
+    $("#desc").val("");
+    $("#point").val("");
+    current_li++;
+    console.log(data)
 });
 
+//Delete li
 $('ul').on('click', '.fa-trash-can',function(){
-    $("#"+$(this).attr('lid')).remove();
-})
+    console.log($("#"+$(this).attr('data')).val());
+    data.splice($("#"+$(this).attr('data')),1)
+    $("#"+$(this).attr('data')).remove();
+    console.log(data)
+});
 
-$('ul').on('click', '.fa-trash-can',function(){
-    $("#"+$(this).attr('lid')).remove();
+//Update li
+$('ul').on('click', '.fa-pen',function(){
+    $("#title").val(""+data[this.id][2]);
+    $("#desc").val(""+data[this.id][3]);
+    $("#point").val(""+data[this.id][4]);
 })
